7f5fdbe3e3f87e813c2b90c02c4ffb611835a711
Youcef Mammar <youcef.mammar@fr.ibm.com>
2014-04-14 11:28:46 +0200

    added a sample for StarRating widget

---
+++ samples/StarRating/app.js
@@ -0,0 +1,37 @@
+require([
+		"dcl/dcl",
+		"dojo/has",
+		"delite/register",
+		"angular-delite/samples/StarRating/ngStarRating",
+		"angular/angular",
+		"angular/resource",
+		"dojo/domReady!"
+		], function(dcl, has, register, ngStarRating){
+
+			var app = angular.module("app", [
+				"StarRating", 
+				"StarRating1", 
+				"StarRating2",
+				]); 
+
+			// a controller was added for debugging, not necessary
+			app.controller("starrater", 
+				function ($scope) {
+					$scope.value = 3
+					$scope.max = 6
+				});
+
+			
+
+			// boostrapping my app on a particular DOM element
+			angular.element(document).ready(function () {
+				var wrapper = document.getElementById('app');
+				angular.bootstrap(wrapper, ["app"]);
+
+				// it appears that this must be triggered only at the very end
+				register.parse();
+			});
+		});
+
+
+
