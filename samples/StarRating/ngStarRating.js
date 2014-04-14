a408aa1c080106480551f240dc703fda6b8b8681
Youcef Mammar <youcef.mammar@fr.ibm.com>
2014-04-14 11:45:02 +0200

    minor reformatting to comply with coding style guidelines

--- samples/StarRating/ngStarRating.js
+++ samples/StarRating/ngStarRating.js
@@ -1,8 +1,8 @@
 define([
-    "angular-delite/ngWidget",
-    "deliteful/StarRating",
-    ], function (ngWidget, StarRating) {
-        ngWidget("StarRating", StarRating);
-        ngWidget("StarRating1", StarRating, {value: "="});
-        ngWidget("StarRating2", StarRating, {value:"=", max: "="});
+	"angular-delite/ngWidget",
+	"deliteful/StarRating",
+	], function (ngWidget, StarRating) {
+		ngWidget("StarRating", StarRating);
+		ngWidget("StarRating1", StarRating, {value: "="});
+		ngWidget("StarRating2", StarRating, {value:"=", max: "="});
     });
