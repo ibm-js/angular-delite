8991200ad30e3c2832a6eb4b81b3682c772b2996
Youcef Mammar <youcef.mammar@fr.ibm.com>
2014-04-15 10:37:56 +0200

    Small change in ngStore
    Now a intermediate class is created to use the angular promises $q

--- ngStore.js
+++ ngStore.js
@@ -1,8 +1,22 @@
-define([], function () {
+define(["dojo/_base/declare"], function (declare) {
 	return function (name, store) {
+
+
 		angular.module("ng" + name, []) 
-			.factory(name, function () {
-				return store;
+			.factory(name, function ($q) {
+
+				var _store = declare(store, {
+					get: function (id) {
+						var d = $q.defer();
+						this.inherited(arguments).then(function (data) {
+							d.resolve(data)	
+						});
+						return d.promise;
+					}
+				});
+				
+
+				return _store;
 			});
 	};
 });
