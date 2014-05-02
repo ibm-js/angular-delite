d6818acd7105f0d6c6b0c56b39d6741b4f675a5a
Youcef Mammar <youcef.mammar@fr.ibm.com>
2014-05-02 15:19:18 +0200

    reformatting and clean up

--- ngWidget.js
+++ ngWidget.js
@@ -1,50 +1,48 @@
 define([], function () {
 
-
 	// helpers
 	var isUndefined = function (value) {
 		return typeof value === "undefined";
 	};
 
 	var overwriteObject = function (obj1, obj2) {
-		Object.keys(obj2).forEach(function(p){
+		Object.keys(obj2).forEach(function (p) {
 			obj1[p] = obj2[p];
 		});
 		return obj1;
 	};
 
-	var diffArray = function(all, excluded){
-		var isIncluded = function (property) { 
-			return (excluded.indexOf(property) === -1); 
+	var diffArray = function (all, excluded) {
+		var isIncluded = function (property) {
+			return (excluded.indexOf(property) === -1);
 		};
 		return all.filter(isIncluded);
 	};
 
-	// properties of a widget
-	var getDefaultProps = function (constructor) {
+	// proprieties of a widget
+	var getDefaultProps = function (Constructor) {
 		// getting all the props
-		var instance = new constructor();
-		var allprops = Object.getPrototypeOf(instance)._getProps();
+		var instance = new Constructor();
+		var all = Object.getPrototypeOf(instance)._getProps();
 		instance.destroy();
 
-		// filtring relevant props
+		// filtering relevant props
 		var excluded = ["baseClass", "focused", "widgetId", "invalidProperties", "invalidRendering"];
-		var props = diffArray(allprops, excluded);
-		return props;
+		return diffArray(all, excluded);
 	};
 
 	var defaultScope = function (props) {
 		var s = {};
-		props.forEach(function(p){
-			s[p] = '@';
+		props.forEach(function (p) {
+			s[p] = "@";
 		});
 		return s;
 	};
 
-	var setIsolatedScope = function (constructor, overwrite) {
-		var myprops = getDefaultProps(constructor);
-		var s = defaultScope(myprops);
-		if (typeof overwrite === "object"){ // if overwrite defined, overwrite the scope
+	var setIsolatedScope = function (Constructor, overwrite) {
+		var props = getDefaultProps(Constructor);
+		var s = defaultScope(props);
+		if (typeof overwrite === "object") { // if overwrite defined, overwrite the scope
 			overwriteObject(s, overwrite);
 		}
 		return s;
@@ -53,72 +51,74 @@ define([], function () {
 	var setTypedValue = function (instance, property, value) {
 		// TODO: this should really be made more robust
 		// also see if there is a way to use https://github.com/ibm-js/delite/blob/master/CustomElement.js#L102
-		switch(typeof instance[property]) {
-			case "number": var asNumber = parseFloat(value);
-						   if (! isNaN(asNumber)){
-							   return asNumber;
-						   }
-			default: return value;
+		switch (typeof instance[property]) {
+			case "number":
+				return value - 0;
+			default:
+				return value;
 		}
 	};
 
 	var setWatchers = function (scope, props) {
-		Object.keys(props).forEach(function(p){
+		Object.keys(props).forEach(function (p) {
 			// if scope property changes, update widget property
 			scope.$watch(p, function (newValue) {
-				if (! isUndefined(newValue)){
+				if (!isUndefined(newValue)) {
 					scope.widget[p] = newValue;
 				}
 			});
 
 			// if widget property changes, update scope property
-			scope.widget.watch(p, function(){
+			scope.widget.watch(p, function () {
 				scope[p] = scope.widget[p];
-				setTimeout(function () { scope.$apply(); }, 1);
+				setTimeout(function () {
+					scope.$apply();
+				}, 1);
 			});
 		});
 	};
 
-	var getId = function(attrs){
+	var getId = function (attrs) {
 		var allowed = ["id", "data-id", "x-id"];
-		for (a in attrs){
-			if (allowed.indexOf(a) > -1) { return attrs[a]; }
+		for (var a in attrs) {
+			if (attrs.hasOwnProperty(a)
+				&& allowed.indexOf(a) > -1) {
+				return attrs[a];
+			}
 		}
 		return null;
-	}
+	};
 
-	return function (name, constructor, overwrite) {
+	return function (name, Constructor, overwrite) {
 		var directiveName = "ng" + name;
-		var isolatedScope = setIsolatedScope(constructor, overwrite);
+		var isolatedScope = setIsolatedScope(Constructor, overwrite);
 		return angular.module(name, []).directive(directiveName, function () {
-				var directive = {
-					restrict: 'E',
-					controller: function ($scope) { },
-					scope: isolatedScope,
-					link: function (scope, elem, attrs) {
+			return {
+				restrict: "E",
+				scope: isolatedScope,
+				link: function (scope, elem, attrs) {
 
-						// insert widget in angular directive
-						scope.widget = new constructor();
-						elem.append(scope.widget);
+					// insert widget in angular directive
+					scope.widget = new Constructor();
+					elem.append(scope.widget);
 
-						// make widget available in parent scope if `id` was given
-						if (! isUndefined(scope.$parent)) {
-							var id = getId(attrs);
-							if (id !== null) {
-								scope.$parent[id] = scope.widget;
-							}
+					// make widget available in parent scope if `id` was given
+					if (!isUndefined(scope.$parent)) {
+						var id = getId(attrs);
+						if (id !== null) {
+							scope.$parent[id] = scope.widget;
 						}
-
-						// process attrs values to their correct type
-						for (a in attrs.$attr){
-							attrs[a] = setTypedValue(scope.widget, a, attrs[a]);
-						}
-
-						// bind widget propreties with directive scope
-						setWatchers(scope, isolatedScope);
 					}
-				};
-				return directive;
-			});
+
+					// process attrs values to their correct type
+					Object.keys(attrs.$attr).forEach(function (a) {
+						attrs[a] = setTypedValue(scope.widget, a, attrs[a]);
+					});
+
+					// bind widget proprieties with directive scope
+					setWatchers(scope, isolatedScope);
+				}
+			};
+		});
 	};
 });
