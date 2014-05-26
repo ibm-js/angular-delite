9631542c4cb2fd58e797fdce33ca2b768e177f4c
Youcef Mammar <youcef.mammar@fr.ibm.com>
2014-05-26 13:03:53 +0200

    Added js doc to store.js

--- wrappers/store.js
+++ wrappers/store.js
@@ -1,34 +1,44 @@
 define(["dojo/_base/declare"], function (declare) {
-	return function (store, q) {
-		var promise = function(){
-			return function(){
-				var d = q.defer();
-				var res = this.inherited(arguments);
-				res.then(function () {             // success
-					d.resolve.apply(d, arguments);
-				}, function(){                     // error
-					d.reject.apply(d, arguments);
-				}, function () {                   // update
-					d.notify.apply(d, arguments);
-				});
-				// TODO: no access is provided to dojo's promise methods
-				// (isRejected, isCancelled, etc.) It may be necessary.
-				return d.promise;
-			};
+
+	/**
+	 * wraps a dojo promise inside an angular promise
+	 */
+	var promise = function(q){
+		return function(){
+			var d = q.defer();
+			var res = this.inherited(arguments);
+			res.then(function () {             // success
+				d.resolve.apply(d, arguments);
+			}, function(){                     // error
+				d.reject.apply(d, arguments);
+			}, function () {                   // update
+				d.notify.apply(d, arguments);
+			});
+			// TODO: no access is provided to dojo's promise methods
+			// (isRejected, isCancelled, etc.) It may be necessary.
+			return d.promise;
 		};
+	};
+
+	/**
+	 * wraps a store constructor with angular promises
+	 * @param {module:dstore/*} Constructor A store constructor.
+	 * @param {angular.$q} q Promise service of angular
+	 */
+	return function (/*Store*/ Constructor, /*angular.$q*/ q) {
 		
-		var _store = store;
+		var _Constructor = Constructor;
 
 		if (typeof q !== "undefined") {
-			_store = declare(store, {
-				get    : promise(),
-				save   : promise(),
-				add    : promise(),
-				put    : promise(),
-				remove : promise()
+			_Constructor = declare(Constructor, {
+				get    : promise(q),
+				save   : promise(q),
+				add    : promise(q),
+				put    : promise(q),
+				remove : promise(q)
 			});
 		}
 		
-		return _store;
+		return _Constructor;
 	};
 });
